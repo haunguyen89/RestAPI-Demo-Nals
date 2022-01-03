16639b87e6cef8b17819e9f2adf9406bff507456
hau nguyen <ncthau@gmail.com>
2022-01-03 22:02:46 +0700

    Deploy angular app

--- src/main/angular/server.js
+++ src/main/angular/server.js
@@ -1,24 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/angular'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/angular/index.html'));
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/angular/'}),
+);
 
-app.use(forceSSL());
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
